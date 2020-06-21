2e19f728d6d1424d4c306751e9a06c75005327c0
Shults Pavel <bmstustudent@yandex.ru>
2020-06-21 18:04:39 +0300

    Первое ревью

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const popup = document.querySelector('.popup-edit')
+const popupOpenButton = document.querySelector('.profile__button-edit')
+const popupCloseButton = popup.querySelector('.popup-edit__close')
+let formElement = popup.querySelector('.popup-edit__form');
+let nameInput = popup.querySelector('.popup-edit__type-name');
+let jobInput = popup.querySelector('.popup-edit__type-about');
+let profName = document.querySelector('.profile__name');
+let profText = document.querySelector('.profile__job');
+
+
+const popupToggle = function(event) {
+    if (!popup.classList.contains('popup_opened')) {
+        nameInput.value = profName.textContent;
+        jobInput.value = profText.textContent;
+    }
+    popup.classList.toggle('popup_opened');
+};
+
+const closePopupOverlay = function(event) {
+    if (event.target !== event.currentTarget) { return }
+    popupToggle()
+}
+
+popupOpenButton.addEventListener('click', popupToggle)
+popupCloseButton.addEventListener('click', popupToggle)
+popup.addEventListener('click', closePopupOverlay)
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profName.textContent = nameInput.value;
+    profText.textContent = jobInput.value;
+    popupToggle()
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
